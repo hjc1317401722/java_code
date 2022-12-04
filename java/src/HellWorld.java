352edbff0c3797fb426dd86e8eba1ff964818357
Administrator <hjc1317401722@163.com>
2022-12-04 22:47:17 +0800

    注释更新

---
+++ java/src/HellWorld.java
@@ -0,0 +1,18 @@
+public class HellWorld {
+    //  单行注释
+    /*
+    多行注释
+    方法快捷键main回车
+    打印快捷键sout回车
+    */
+    /**
+     * 文档注释，描述方法的作用
+     * @param args 参数说明
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+
+
